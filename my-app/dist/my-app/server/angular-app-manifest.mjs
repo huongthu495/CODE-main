617314259d2e89e3ed2649ca40b8739326b0866b
Trần Hạc Hương Thư <thuthh234112e@st.uel.edu.vn>
2026-08-17 09:35:01 +0700

    change

--- my-app/dist/my-app/server/angular-app-manifest.mjs
+++ my-app/dist/my-app/server/angular-app-manifest.mjs
@@ -7,184 +7,184 @@ export default {
   routes: [
   {
     "renderMode": 0,
-    "route": "/CODE-main"
+    "route": "/"
   },
   {
     "renderMode": 0,
-    "route": "/CODE-main/sac-blog"
+    "route": "/sac-blog"
   },
   {
     "renderMode": 0,
-    "route": "/CODE-main/contact"
+    "route": "/contact"
   },
   {
     "renderMode": 0,
-    "route": "/CODE-main/policy"
+    "route": "/policy"
   },
   {
     "renderMode": 0,
-    "route": "/CODE-main/privacy"
+    "route": "/privacy"
   },
   {
     "renderMode": 0,
-    "route": "/CODE-main/term"
+    "route": "/term"
   },
   {
     "renderMode": 0,
-    "route": "/CODE-main/ship"
+    "route": "/ship"
   },
   {
     "renderMode": 0,
-    "route": "/CODE-main/ship-method"
+    "route": "/ship-method"
   },
   {
     "renderMode": 0,
-    "route": "/CODE-main/how-to-buy"
+    "route": "/how-to-buy"
   },
   {
     "renderMode": 0,
-    "route": "/CODE-main/aboutus"
+    "route": "/aboutus"
   },
   {
     "renderMode": 0,
-    "route": "/CODE-main/about-us"
+    "route": "/about-us"
   },
   {
     "renderMode": 0,
-    "route": "/CODE-main/products"
+    "route": "/products"
   },
   {
     "renderMode": 0,
-    "route": "/CODE-main/products/*"
+    "route": "/products/*"
   },
   {
     "renderMode": 0,
-    "route": "/CODE-main/login"
+    "route": "/login"
   },
   {
     "renderMode": 0,
-    "route": "/CODE-main/signup"
+    "route": "/signup"
   },
   {
     "renderMode": 0,
-    "route": "/CODE-main/forgot-password"
+    "route": "/forgot-password"
   },
   {
     "renderMode": 0,
-    "route": "/CODE-main/reset-password"
+    "route": "/reset-password"
   },
   {
     "renderMode": 0,
-    "redirectTo": "/CODE-main/account/profile",
-    "route": "/CODE-main/account"
+    "redirectTo": "/account/profile",
+    "route": "/account"
   },
   {
     "renderMode": 0,
-    "route": "/CODE-main/account/profile"
+    "route": "/account/profile"
   },
   {
     "renderMode": 0,
-    "route": "/CODE-main/account/address"
+    "route": "/account/address"
   },
   {
     "renderMode": 0,
-    "route": "/CODE-main/account/orders"
+    "route": "/account/orders"
   },
   {
     "renderMode": 0,
-    "route": "/CODE-main/account/wishlist"
+    "route": "/account/wishlist"
   },
   {
     "renderMode": 0,
-    "redirectTo": "/CODE-main/account/profile",
-    "route": "/CODE-main/account/**"
+    "redirectTo": "/account/profile",
+    "route": "/account/**"
   },
   {
     "renderMode": 0,
-    "route": "/CODE-main/cart"
-  },
-  {
-    "renderMode": 0,
-    "preload": [
-      "chunk-RR3KSJIO.js"
-    ],
-    "route": "/CODE-main/admin"
+    "route": "/cart"
   },
   {
     "renderMode": 0,
     "preload": [
       "chunk-RR3KSJIO.js"
     ],
-    "route": "/CODE-main/admin/mainpage"
+    "route": "/admin"
   },
   {
     "renderMode": 0,
     "preload": [
       "chunk-RR3KSJIO.js"
     ],
-    "route": "/CODE-main/admin/orders"
+    "route": "/admin/mainpage"
   },
   {
     "renderMode": 0,
     "preload": [
       "chunk-RR3KSJIO.js"
     ],
-    "route": "/CODE-main/admin/users"
+    "route": "/admin/orders"
   },
   {
     "renderMode": 0,
     "preload": [
       "chunk-RR3KSJIO.js"
     ],
-    "route": "/CODE-main/admin/products"
+    "route": "/admin/users"
   },
   {
     "renderMode": 0,
     "preload": [
       "chunk-RR3KSJIO.js"
     ],
-    "route": "/CODE-main/admin/blogs"
+    "route": "/admin/products"
   },
   {
     "renderMode": 0,
     "preload": [
       "chunk-RR3KSJIO.js"
     ],
-    "route": "/CODE-main/admin/feedbacks"
+    "route": "/admin/blogs"
   },
   {
     "renderMode": 0,
-    "route": "/CODE-main/product/*"
+    "preload": [
+      "chunk-RR3KSJIO.js"
+    ],
+    "route": "/admin/feedbacks"
   },
   {
     "renderMode": 0,
-    "route": "/CODE-main/collections/*"
+    "route": "/product/*"
   },
   {
     "renderMode": 0,
-    "route": "/CODE-main/blogs"
+    "route": "/collections/*"
   },
   {
     "renderMode": 0,
-    "route": "/CODE-main/blogs/*"
+    "route": "/blogs"
   },
   {
     "renderMode": 0,
-    "route": "/CODE-main/blog-catalog"
+    "route": "/blogs/*"
   },
   {
     "renderMode": 0,
-    "route": "/CODE-main/letters"
+    "route": "/blog-catalog"
   },
   {
     "renderMode": 0,
-    "route": "/CODE-main/blog/*"
+    "route": "/letters"
+  },
+  {
+    "renderMode": 0,
+    "route": "/blog/*"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 9641, hash: '375e8920f02fd4c3430ea94814e300609ec926eec2e389043a810eba03d0bcda', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 9989, hash: '9cfb8668116b17299ee33d8eccf8a7a052a5135619b48a15f2003dec3def2647', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 9621, hash: '7467e93e361267a92e495a8f3c0b2b673bc5b874797e1bbd605c6ca00f42d604', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 9969, hash: '1cbf9af4ba3102282bbaf839402e7979ea19ee283f3a74c21dbdeb9903f286f6', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-3SEBDQFR.css': {size: 40, hash: 'RzXWn47xU0o', text: () => import('./assets-chunks/styles-3SEBDQFR_css.mjs').then(m => m.default)}
   },
 };
